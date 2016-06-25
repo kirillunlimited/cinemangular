ae742fdab2975a93499a03070df66a93358d6a70
Kirill Ivanov <kirillunlimited@gmail.com>
2016-06-25 16:22:59 +0300

    search button ng-disabled if input empty

--- src/app/controllers/search.controller.js
+++ src/app/controllers/search.controller.js
@@ -2,24 +2,27 @@
 module.exports = function SearchController(jsonApi) {
   var vm = this;
 
+  vm.searchString = "";
   vm.searchFilter = "films";
   vm.status = "Ready";
 
   vm.findFilm = function() {
-    vm.status = "Loading";
-    if (vm.searchFilter == "films") {
-      jsonApi.fetch("http://api.kinopoisk.cf/searchFilms?keyword=" + vm.searchString).then(function(filmsResponse) {
-        vm.filmsContent = filmsResponse.data.searchFilms;
-        vm.peopleContent = "";
-        vm.status = "Ready";
-      });
-    }
-    else {
-      jsonApi.fetch("http://api.kinopoisk.cf/searchPeople?keyword=" + vm.searchString).then(function(peopleResponse) {
-        vm.peopleContent = peopleResponse.data.searchPeople;
-        vm.filmsContent = "";
-        vm.status = "Ready";
-      });
+    if (vm.searchString != "") {
+      vm.status = "Loading";
+      if (vm.searchFilter == "films") {
+        jsonApi.fetch("http://api.kinopoisk.cf/searchFilms?keyword=" + vm.searchString).then(function(filmsResponse) {
+          vm.filmsContent = filmsResponse.data.searchFilms;
+          vm.peopleContent = "";
+          vm.status = "Ready";
+        });
+      }
+      else {
+        jsonApi.fetch("http://api.kinopoisk.cf/searchPeople?keyword=" + vm.searchString).then(function(peopleResponse) {
+          vm.peopleContent = peopleResponse.data.searchPeople;
+          vm.filmsContent = "";
+          vm.status = "Ready";
+        });
+      }
     }
   };
 
