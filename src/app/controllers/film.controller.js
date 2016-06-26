30d4139d00b24df5261511516560ebc8931f04ac
Kirill Ivanov <kirillunlimited@gmail.com>
2016-06-26 17:29:29 +0300

    ng if updates on every page

--- src/app/controllers/film.controller.js
+++ src/app/controllers/film.controller.js
@@ -1,13 +1,19 @@
 'use strict';
-module.exports = function FilmController(jsonApi, $routeParams) {
+module.exports = function FilmController(jsonApi, dateService, $routeParams) {
   var vm = this;
 
   vm.status = "Loading";
 
-  jsonApi.fetch("http://api.kinopoisk.cf/getFilm?filmID=" + $routeParams.filmID).then(function(response) {
-    vm.filmContent = response.data;
+  jsonApi.fetch("http://api.kinopoisk.cf/getFilm?filmID=" + $routeParams.filmID).then(function(filmResponse) {
+    vm.filmContent = filmResponse.data;
     console.log(vm.filmContent);
     vm.posterPath = jsonApi.switchPosterSize(vm.filmContent.posterURL, 360);
-    vm.status = "Ready";
+    jsonApi.fetch("http://api.kinopoisk.cf/getSeance?filmID=" + $routeParams.filmID + "&date=" + dateService.getToday()).then(function(seancesResponse) {
+      vm.isInCinema = false;
+      if (seancesResponse.data.items) {
+        vm.isInCinema = true;
+      }
+      vm.status = "Ready";
+    });
   });
 };
