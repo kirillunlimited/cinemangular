80107383f157714cdf570ca1722ef0481cf5946a
Kirill Ivanov <kirillunlimited@gmail.com>
2017-05-03 00:36:45 +0300

    Remove cinema

--- src/app/controllers/index.js
+++ src/app/controllers/index.js
@@ -4,6 +4,5 @@ var app = require('angular').module('app');
 app.controller('AfishaController', require('./afisha.controller.js'));
 app.controller('FilmController', require('./film.controller.js'));
 app.controller('PersonController', require('./person.controller.js'));
-app.controller('CinemaController', require('./cinema.controller.js'));
 app.controller('SearchController', require('./search.controller.js'));
 app.controller('CreditsController', require('./credits.controller.js'));
